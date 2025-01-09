61e24902ce03b41b25167c17b4bd615b9c9ad9b5
Dan Obregon <dan.obregon@destpet.com>
2025-01-09 10:01:00 -0600

    initial commit

---
+++ app/components/app-header.vue
@@ -0,0 +1,8 @@
+<template>
+  <header class="flex justify-between items-center mt-10">
+    <NuxtLink to="/" class="text-xl font-bold"> Finance Tracker </NuxtLink>
+    <div>
+      <UAvatar src="https://avatars.githubusercontent.com/u/26966537?v=4" />
+    </div>
+  </header>
+</template>
