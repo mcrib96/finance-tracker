ced6cf867baf679e700b59530f6c4e98b364ea7f
Dan Obregon <dan.obregon@destpet.com>
2025-01-09 14:19:51 -0600

    package.json: +supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"],
+  modules: ["@nuxt/ui", "@nuxtjs/supabase"],
   future: {
     compatibilityVersion: 4,
+  },
+  supabase: {
+    redirect: false,
   },
 });
