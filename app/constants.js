b42c7daca3be79f178d6ee1dbfcaa3dbf46f4da3
Dan Obregon <dan.obregon@destpet.com>
2025-01-10 10:29:14 -0600

    constants: +category, type options

--- app/constants.js
+++ app/constants.js
@@ -1,1 +1,3 @@
 export const transactionViewOptions = ["Yearly", "Monthly", "Daily"];
+export const categoryOptions = ["Food", "Housing", "Car", "Entertainment"];
+export const typeOptions = ["Income", "Expense", "Investment", "Saving"];
